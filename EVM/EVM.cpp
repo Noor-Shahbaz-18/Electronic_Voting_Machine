2aa06316ab30d86971ad79bdb52d8fece741ec53
ShehzalIrfan <shehzalirfan@gmail.com>
2025-04-20 20:27:53 -0700

    code

--- EVM/EVM.cpp
+++ EVM/EVM.cpp
@@ -10,21 +10,26 @@ protected:
     string username;
     string password;
 public:
-    User(string u, string p) : username(u), password(p) {} // constructor initializer list
+    User(string u, string p) : username(u), password(p) {} // constructor initializor list
      bool login(const string& user, const string& pass) {  // function to check whether correct(pass or user)
         return username == user && password == pass;
-     }
+    }
+
     virtual void showMenu() = 0; // pure virtual function
 };
 class Admin : public User {
 public:
     Admin(string u, string p) : User(u, p) {}
-    void showMenu() {
-        cout << "Welcome Admin: " << username << endl;
-        cout << "1. Create Election" << endl;
-        cout << "2. View Results" << endl;
-        cout << "3. End Election" << endl;
+    void displayMenu()
+    {
+        cout << "\nAdmin Menu:\n";
+        cout << "1. Create Election\n";
+        cout << "2. Add Candidate\n";
+        cout << "3. View Results\n";
+        cout << "4. Logout\n";
+        cout << "Enter choice: ";
     }
+
     void createElection() {
         cout << "Creating Election..." << endl;
     }
@@ -47,12 +52,8 @@ public:
 };
 class Voter : public User {
     int voterId;
-    string unionCouncilCode;
 public:
-    Voter(string u, string p, int id, string code) : User(u, p), voterId(id), unionCouncilCode(code) {}
-    string getUnionCouncilCode() { 
-        return unionCouncilCode; 
-    }
+    Voter(string u, string p, int id) : User(u, p), voterId(id) {}
     void showMenu() {
         cout << "Welcome Voter: " << username << endl;
         cout << "1. View Elections" << endl;
@@ -82,8 +83,7 @@ public:
             getline(ss, username, ',');
             getline(ss, password, ',');
             ss >> id;
-            getline(ss, code, ',');
-            voters[numVoters++] = Voter(username, password, id, code);
+            voters[numVoters++] = Voter(username, password, id);
         }
         file.close();
     }
@@ -124,120 +124,6 @@ public:
         return Candidate(name, party);
     }
 };
-#include<iostream>
-#include<string>
-#include <fstream>
-using namespace std;
-// ---------- Base Election Class ----------
-class Election {
-protected:
-    string title;
-    Candidate candidates[100];
-    Voter voters[100];
-    int candidateCount;
-    int voterCount;
 
-public:
-    Election(string title) : title(title), candidateCount(0), voterCount(0) {}
-    virtual void showElectionType() {
-        cout << "Generic Election" << endl;
-    }
-    void addCandidate(string name, int id)
-    {
-        candidates[candidateCount++] = Candidate(name, id);
-        cout << "  - Candidate '" << name << "' added successfully." << endl;
-    }
-    void showCandidates()
-    {
-        cout << "\n------------ Candidates ------------\n";
-        for (int i = 0; i < candidateCount; i++) {
-            cout << "ID: " << candidates[i].getId()
-                << " | Name: " << candidates[i].getName() << endl;
-        }
-    }
-    void loadVotersFromFile(const string& filename)
-    {
-        ifstream file(filename);
-        if (!file)
-        {
-            cout << "Error: Cannot open file " << filename << endl;
-            return;
-        }
-        string name, cnic, postal;
-        int id;
-        while (getline(file, name, ',')) {
-            file >> id;
-            file.ignore();
-            getline(file, cnic, ',');
-            getline(file, postal);
-            voters[voterCount++] = Voter(name, id, cnic, postal);
-        }
-        file.close();
-        cout << " Voter data loaded successfully.\n";
-    }
-
-
-    void secureVote(string cnic, string postalCode, int candidateId)
-    {
-        bool voterFound = false;
-
-        for (int i = 0; i < voterCount; i++)
-        {
-            if (voters[i].cnic == cnic && voters[i].postalCode == postalCode)
-            {
-                voterFound = true;
-
-                if (voters[i].hasVoted)
-                {
-                    cout << "\n You have already voted!" << endl;
-                    return;
-                }
-
-                bool candidateFound = false;
-                for (int j = 0; j < candidateCount; j++)
-                {
-                    if (candidates[j].getId() == candidateId)
-                    {
-                        candidates[j].addVote();
-                        voters[i].hasVoted = true;
-                        cout << "\n Vote cast for " << candidates[j].getName() << "!\n";
-                        candidateFound = true;
-                        break;
-                    }
-                }
-
-                if (!candidateFound)
-                {
-                    cout << "\n Invalid Candidate ID!\n";
-                }
-
-                return;
-            }
-        }
-        if (!voterFound) {
-            cout << "\n CNIC or Postal Code did not match our records!\n";
-        }
-    }
-    void showResults()
-    {
-        cout << "\n------------ Election Results: " << title << " ------------\n";
-        for (int i = 0; i < candidateCount; i++) {
-            cout << setw(20) << left << candidates[i].getName()
-                << " | Votes: " << candidates[i].votes << endl;
-        }
-    }
-    void showWinner() {
-        int maxVotes = -1;
-        string winner = "None";
-        for (int i = 0; i < candidateCount; i++) {
-            if (candidates[i].votes > maxVotes) {
-                maxVotes = candidates[i].votes;
-                winner = candidates[i].getName();
-            }
-        }
-        cout << "\n Winner: " << winner << " with " << maxVotes << " votes!" << endl;
-    }
-
-};
 
 
